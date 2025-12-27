53a57bd4f3fdbcf43cc226b070dc04d527c7b745
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2025-12-27 14:50:06 +0000

    Extend schema to manage field constraints and validators

--- schemas.py
+++ schemas.py
@@ -1,40 +1,44 @@
 from datetime import datetime
 from typing import Annotated, Literal
+from uuid import uuid4
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl, IPvAnyAddress, PositiveInt
 
 VoicePresets = Literal["v2/en_speaker_1", "v2/en_speaker_9"]
 
 
 class ModelRequest(BaseModel):
-    prompt: str
+    prompt: Annotated[str, Field(min_length=1, max_length=1000)]
 
 
 class ModelResponse(BaseModel):
-    request_id: str
-    ip: str | None
-    content: str | None
+    request_id: Annotated[str, Field(default_factory=lambda: uuid4().hex)]
+    # No default value for ip to allow for None - raise validation error if None or no valid IP address is provided
+    ip: Annotated[str, IPvAnyAddress] | None
+    content: Annotated[str | None, Field(min_length=0, max_length=100000)]
     created_at: datetime = datetime.now()
 
 
 class TextModelRequest(ModelRequest):
     model: Literal["gpt-4o-mini", "gpt-4o"]
-    temperature: float = 0.0
+    temperature: Annotated[float, Field(ge=0.0, le=1.0, default=0.0)]
 
 
 class TextModelResponse(ModelResponse):
-    tokens: int
+    tokens: Annotated[int, Field(ge=0)]
 
 
-ImageSize = Annotated[tuple[int, int], "Width and height of the image in pixels"]
+ImageSize = Annotated[
+    tuple[PositiveInt, PositiveInt], "Width and height of the image in pixels"
+]
 
 
 class ImageModelRequest(ModelRequest):
     model: Literal["dall-e-3", "dall-e-2"]
     output_size: ImageSize
-    num_inference_steps: int = 20
+    num_inference_steps: Annotated[int, Field(ge=0, le=2000, default=20)]
 
 
 class ImageModelResponse(ModelResponse):
     size: ImageSize
-    url: str
+    url: Annotated[str, HttpUrl] | None = None
