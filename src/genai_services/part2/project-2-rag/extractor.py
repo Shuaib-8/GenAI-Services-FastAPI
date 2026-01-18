b8aba390b16d55b6d4c1ffbd4648bdca32d0016a
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2026-01-18 07:59:08 +0000

    Setup pdf text extractor

---
+++ src/genai_services/part2/project-2-rag/extractor.py
@@ -0,0 +1,13 @@
+from pypdf import PageObject, PdfReader
+
+
+def pdf_text_extractor(filepath: str) -> None:
+    """Extract text from a PDF file."""
+    content = ""
+    pdf_reader = PdfReader(filepath, strict=True)
+    for page in pdf_reader.pages:
+        page_text: PageObject = page
+        if page_text:
+            content += f"{page_text}\n\n"
+    with open(filepath.replace("pdf", "txt"), "w", encoding="utf-8") as file:
+        file.write(content)
