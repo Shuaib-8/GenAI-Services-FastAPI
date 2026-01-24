89b6dc36131919410a3c6ad82fd115f734b027b0
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2026-01-24 12:04:17 +0000

    Sreamline pdf_text_extractor function to execute via page.extract_text()

--- src/genai_services/part2/project_2_rag/extractor.py
+++ src/genai_services/part2/project_2_rag/extractor.py
@@ -1,4 +1,4 @@
-from pypdf import PageObject, PdfReader
+from pypdf import PdfReader
 
 
 def pdf_text_extractor(filepath: str) -> None:
@@ -6,7 +6,7 @@ def pdf_text_extractor(filepath: str) -> None:
     content = ""
     pdf_reader = PdfReader(filepath, strict=True)
     for page in pdf_reader.pages:
-        page_text: PageObject = page
+        page_text: str = page.extract_text()
         if page_text:
             content += f"{page_text}\n\n"
     with open(filepath.replace("pdf", "txt"), "w", encoding="utf-8") as file:
