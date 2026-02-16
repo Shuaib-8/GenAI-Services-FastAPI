9e25b2556649765bbbcf62ee4db523be21121bbf
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2026-02-16 08:51:18 +0000

    Better illustration of async vs sync wall time

--- src/genai_services/part2/async.py
+++ src/genai_services/part2/async.py
@@ -2,16 +2,38 @@ import asyncio
 import time
 
 
-async def task():
+def sync_task():
+    print("Start of sync task")
+    time.sleep(5)
+    print("Task resumed after 5 seconds")
+
+
+async def async_task():
     print("Start of async task")
     await asyncio.sleep(5)
     print("Task resumed after 5 seconds")
 
 
+start_time = time.time()
+for _ in range(3):
+    sync_task()
+end_time = time.time()
+print(f"\nTime taken for sync tasks: {end_time - start_time} seconds")
+
+
 # would've taken 50 (5 seconds x 10 tasks) seconds to complete if we had not used asyncio.gather
 async def spawn_tasks():
     await asyncio.gather(
-        task(), task(), task(), task(), task(), task(), task(), task(), task(), task()
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
+        async_task(),
     )
 
 
@@ -19,4 +41,4 @@ start_time = time.time()
 asyncio.run(spawn_tasks())
 end_time = time.time()
 
-print(f"Time taken: {end_time - start_time} seconds")
+print(f"\nTime taken for async tasks: {end_time - start_time} seconds")
